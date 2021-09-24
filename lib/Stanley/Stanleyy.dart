55b01b026681c3fdee8fd3b2492cbba12c8ed7ba
StanleyTeja <90432054+StanleyTeja@users.noreply.github.com>
2021-09-24 14:09:37 +0700

    Add files via upload

---
+++ lib/Stanley/Stanleyy.dart
@@ -0,0 +1,56 @@
+// Navigator.push
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: FirstPage(),
+    );
+  }
+}
+
+class FirstPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Halaman Stanley'),
+      ),
+      body: Center(
+        child: RaisedButton(
+          child: Text('LANJUT'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (BuildContext context) {
+                return SecondPage();
+              }),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+class SecondPage extends StatelessWidget  {
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Halaman Teja'),
+      ),
+      body: Center(
+        child: RaisedButton(
+          child: Text('KEMBALI'),
+          onPressed: (){
+            Navigator.pop(context);
+          },
+        ),
+      ),
+    );
+  }
+}
